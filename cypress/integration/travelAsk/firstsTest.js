2b374660961bf0517795f6195ae4e8fc5cd502ab
Alex Mikulin <forgeta04@gmail.com>
2020-08-05 00:02:17 +0300

    comment

---
+++ cypress/integration/travelAsk/firstsTest.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+
+describe('My first Test', () => {
+    it ('Visits the TravelAsk', () =>{
+        cy.visit( 'https://travelask.ru/')
+           
+    })
+})
+ 
+describe('My second test', () =>
+    it ( 'Scroll to the bottom', ()=> {
+        cy.scrollTo('bottom',  { duration: 3000 })
+
+    })
+)
+
+describe('My third test', () =>
+    it ( 'Scroll to the top', ()=> {
+        cy.scrollTo('top', { duration: 3000 })
+
+    })
+)
